d1bb2c7c88403ea05ec39d1c6a3d07c42fb77251
RagnarLodbrok2017 <35165213+RagnarLodbrok2017@users.noreply.github.com>
2018-01-27 17:29:08 +0200

    nav

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,6 +9,7 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
         <!-- Styles -->
         <style>
@@ -21,28 +22,9 @@
                 margin: 0;
             }
 
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
             .content {
                 text-align: center;
+                margin-top: 50px;
             }
 
             .title {
@@ -62,18 +44,168 @@
             .m-b-md {
                 margin-bottom: 30px;
             }
+            .navbar-more .navbar-nav > li > a {
+                color: rgb(64, 64, 64);
+            }
         </style>
     </head>
     <body>
-            <div class="container">
-                <div class="text-center">
-                    <a href="login" class="btn btn-primary"> Login</a>
-                    <a href="signup" class="btn btn-primary"> SignUp</a>
+    <div class="navbar-more-overlay"></div>
+    <nav class="navbar navbar-inverse navbar-fixed-top animate">
+        <div class="container navbar-more visible-xs">
+            <form class="navbar-form navbar-left" role="search">
+                <div class="form-group">
+                    <div class="input-group">
+                        <input type="text" class="form-control" placeholder="Search for...">
+                        <span class="input-group-btn">
+							<button class="btn btn-default" type="submit">Submit</button>
+						</span>
+                    </div>
                 </div>
+            </form>
+            <ul class="nav navbar-nav">
+                <li>
+                    <a href="#">
+                        <span class="menu-icon fa fa-picture-o"></span>
+                        Photos
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <span class="menu-icon fa fa-bell-o"></span>
+                        Reservations
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <span class="menu-icon fa fa-picture-o"></span>
+                        Photos
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <span class="menu-icon fa fa-bell-o"></span>
+                        Reservations
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <span class="menu-icon fa fa-picture-o"></span>
+                        Photos
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <span class="menu-icon fa fa-bell-o"></span>
+                        Reservations
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <span class="menu-icon fa fa-picture-o"></span>
+                        Photos
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <span class="menu-icon fa fa-bell-o"></span>
+                        Reservations
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <span class="menu-icon fa fa-picture-o"></span>
+                        Photos
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <span class="menu-icon fa fa-bell-o"></span>
+                        Reservations
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <span class="menu-icon fa fa-picture-o"></span>
+                        Photos
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <span class="menu-icon fa fa-bell-o"></span>
+                        Reservations
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <span class="menu-icon fa fa-picture-o"></span>
+                        Photos
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <span class="menu-icon fa fa-bell-o"></span>
+                        Reservations
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <div class="container">
+            <div class="navbar-header hidden-xs">
+                <a class="navbar-brand" href="#">Brand</a>
             </div>
+
+            <ul class="nav navbar-nav navbar-right mobile-bar">
+                <li>
+                    <a href="#">
+                        <span class="menu-icon fa fa-home"></span>
+                        Home
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <span class="menu-icon fa fa-info"></span>
+                        <span class="hidden-xs">About the Boat</span>
+                        <span class="visible-xs">About</span>
+                    </a>
+                </li>
+                <li class="hidden-xs">
+                    <a href="#">
+                        <span class="menu-icon fa fa-picture-o"></span>
+                        Photos
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <span class="menu-icon fa fa-ship"></span>
+                        Cruises
+                    </a>
+                </li>
+                <li class="hidden-xs">
+                    <a href="#">
+                        <span class="menu-icon fa fa-bell-o"></span>
+                        Reservations
+                    </a>
+                </li>
+                <li>
+                    <a href="login">
+                        <span class="menu-icon fa fa-phone"></span>
+                        <strong class="hidden-xs">Login</strong>
+                        <span class="visible-xs">Contact</span>
+                    </a>
+                </li>
+                <li class="visible-xs">
+                    <a href="#navbar-more-show">
+                        <span class="menu-icon fa fa-bars"></span>
+                        More
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </nav>
             <div class="content">
                 <div class="title m-b-md">
-                    Cars Look
+                    Cars Agency
                 </div>
             </div>
         </div>
