0442b7b25f683a7ab4472c4a54550d7e45c519f3
RagnarLodbrok2017 <35165213+RagnarLodbrok2017@users.noreply.github.com>
2018-01-28 23:37:17 +0200

    r

--- resources/views/userprofile.blade.php
+++ resources/views/userprofile.blade.php
@@ -2,7 +2,7 @@
 @section('nav')
 @endsection
 <div class="container" style="margin-top: 50px;">
-    <h2><strong> {{Auth::user()->name}} </strong> Profile:</h2>
+    <h2><strong>{{Auth::user()->name}} </strong> Profile:</h2>
     <table class="table table-hover">
         <thead>
         <tr>
@@ -20,11 +20,11 @@
             @foreach($soldcars as $soldcar)
                 <tr>
                     <th scope="row">{{$soldcar->id}}</th>
-                    <td>{{$soldcar->sold_cars_id}}</td>
+                    <td>{{$soldcar->car_id}}</td>
                     <td>{{$soldcar->user_id}}</td>
                     <td>{{$soldcar->total_price}}</td>
-                    <td>{{$soldcar->car->name}}</td>
-                    <td>{{$soldcar->category->name}}</td>
+                    <td>{{$soldcar->car.name}}</td>
+                    <td>{{$soldcar->category.name}}</td>
                     <td>
                         {!! Form::open(['url'=>"carsuser/{$soldcar->id}", 'method'=>'DELETE'])!!}
                         {!! Form::submit('Delete',["class"=>"btn btn-danger"]) !!}
